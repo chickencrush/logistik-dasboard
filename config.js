dd026645bd4b4594148dcf62a5290775f3f77bad
Chickencrush <syandi.chickencrush@gmail.com>
2026-09-20 16:41:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Upload file ini menggantikan config.js lama. Tidak perlu input URL di UI.
  */
 window.DASHBOARD_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzT3RZnCOLP0IdC7hekehWK85Lzc4tLuwx_47HLkKBR_bURKpM_PHDLeYzYwIN_iiTp/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzRxfZK1x_LcrTDBjqQpkGJg7fHG2r9xYNpt4DuxNHFAzANxZLY2TiRej1OznMautyn/exec',
   AUTO_CONNECT: true,
   REFRESH_MINUTES: 5,
   APP_NAME: 'Dashboard Logistik Chicken Crush V4.2'
