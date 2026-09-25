46560eaadd5549399157cd0d4689e9e8d28acc60
Chickencrush <syandi.chickencrush@gmail.com>
2026-09-25 21:50:30 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
-/**
- * Koneksi bawaan CC Logistik V4.2, sudah diisi untuk semua perangkat.
- * Upload file ini menggantikan config.js lama. Tidak perlu input URL di UI.
- */
-window.DASHBOARD_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzRxfZK1x_LcrTDBjqQpkGJg7fHG2r9xYNpt4DuxNHFAzANxZLY2TiRej1OznMautyn/exec',
-  AUTO_CONNECT: true,
-  REFRESH_MINUTES: 5,
-  APP_NAME: 'Dashboard Logistik Chicken Crush V4.2'
-};
+window.DAILY_REPORT_URL = "https://script.google.com/macros/s/AKfycbz94wl1Sg5SiEvnxd0UH5AFMNSSAla63BWHdHYPJ1bJOq0c9K2Pk7sTv4aWjIyqhSj0/exec";
